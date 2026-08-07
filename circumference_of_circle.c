8671ee4133f0ddf7a61035a306af443f78688a10
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-07 08:19:42 +0530

    circumference_of_circle.c

---
+++ circumference_of_circle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main() {
+    float radius, circumference;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    circumference = 2 * PI * radius;
+
+    printf("Circumference of the circle = %.2f", circumference);
+
+    return 0;
+}
