f39b6a9aafd18d1c49a48fb286724085f75c0568
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-09 08:55:45 +0530

    matrix_symmetric.c

---
+++ matrix_symmetric.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    int matrix[10][10];
+    int isSymmetric = 1;
+
+    printf("Enter the size of matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter matrix elements:\n");
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                isSymmetric = 0;
+                break;
+            }
+        }
+
+        if (isSymmetric == 0) {
+            break;
+        }
+    }
+
+    if (isSymmetric == 1) {
+        printf("The matrix is symmetric.");
+    } else {
+        printf("The matrix is not symmetric.");
+    }
+
+    return 0;
+}
