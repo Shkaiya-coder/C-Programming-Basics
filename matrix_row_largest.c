f953b73e6784e6860621f2fe4551ba3417f3e6a0
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-09 09:00:45 +0530

    matrix_row_largest.c

---
+++ matrix_row_largest.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int rows, columns;
+    int matrix[10][10];
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &columns);
+
+    printf("Enter matrix elements:\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Largest element in each row:\n");
+
+    for (int i = 0; i < rows; i++) {
+        int largest = matrix[i][0];
+
+        for (int j = 1; j < columns; j++) {
+            if (matrix[i][j] > largest) {
+                largest = matrix[i][j];
+            }
+        }
+
+        printf("Row %d = %d\n", i + 1, largest);
+    }
+
+    return 0;
+}
