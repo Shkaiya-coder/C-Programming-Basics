fff205b34734689c38700d58c2a4818208bea121
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-09 09:19:11 +0530

    matrix_count_negative.c

---
+++ matrix_count_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int rows, columns;
+    int matrix[10][10];
+    int count = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &columns);
+
+    printf("Enter matrix elements:\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            scanf("%d", &matrix[i][j]);
+
+            if (matrix[i][j] < 0) {
+                count++;
+            }
+        }
+    }
+
+    printf("Number of negative elements = %d", count);
+
+    return 0;
+}
