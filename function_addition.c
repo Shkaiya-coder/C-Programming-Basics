2a2f9a08a7e7bebac944314e03f7cede00bb8f49
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-10 08:24:35 +0530

    function_addition.c

---
+++ function_addition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a, b, result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    result = add(a, b);
+
+    printf("Sum = %d", result);
+
+    return 0;
+}
