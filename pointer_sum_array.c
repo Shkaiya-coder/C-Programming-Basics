d64c37d6e2abcd6706092861b9b16f89e4d95848
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-11 08:48:59 +0530

    pointer_sum_array.c

---
+++ pointer_sum_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int sumArray(int *arr, int size) {
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += *(arr + i);
+    }
+
+    return sum;
+}
+
+int main() {
+    int arr[5];
+    int sum;
+
+    printf("Enter 5 elements:\n");
+
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    sum = sumArray(arr, 5);
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
