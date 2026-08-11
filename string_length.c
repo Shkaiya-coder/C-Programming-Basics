3dcd9c13a5c536ddb5d8ac4044024beac68e694d
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-11 08:51:41 +0530

    string_length.c

---
+++ string_length.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    printf("Enter a string: ");
+    scanf("%99[^\n]", str);
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    printf("Length = %d", length);
+
+    return 0;
+}
