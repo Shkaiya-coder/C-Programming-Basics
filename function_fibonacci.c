bb1e81728d94b34e3475b3a237834a6c8133e4e7
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-11 08:38:34 +0530

    function_fibonacci.c

---
+++ function_fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void fibonacci(int n) {
+    int a = 0, b = 1, next;
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", a);
+
+        next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    fibonacci(n);
+
+    return 0;
+}
