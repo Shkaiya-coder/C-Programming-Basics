2ab72ab5307300169f4e6526fc5463d043896dec
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-11 08:54:32 +0530

    string_concatenate.c

---
+++ string_concatenate.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    char str1[200], str2[100];
+    int i = 0, j = 0;
+
+    printf("Enter first string: ");
+    scanf("%99[^\n]", str1);
+
+    getchar();
+
+    printf("Enter second string: ");
+    scanf("%99[^\n]", str2);
+
+    while (str1[i] != '\0') {
+        i++;
+    }
+
+    while (str2[j] != '\0') {
+        str1[i] = str2[j];
+        i++;
+        j++;
+    }
+
+    str1[i] = '\0';
+
+    printf("Concatenated string = %s", str1);
+
+    return 0;
+}
