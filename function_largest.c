7ead35bfa1ca6af8985e3cf50508eb72bd5a5a31
Shkaiya Ligy <ligyligyreya@gmail.com>
2026-08-11 08:32:32 +0530

    function_largest.c

---
+++ function_largest.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int largest(int a, int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+int main() {
+    int a, b, result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    result = largest(a, b);
+
+    printf("Largest = %d", result);
+
+    return 0;
+}
